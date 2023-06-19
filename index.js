4e7f7dfa6f68354d26171d8d62637f6c9bed9915
19cmackenzie <19cmackenzie@wakatipu.school.nz>
2023-06-19 22:20:46 +1200

    proof of concept
    Created a basic lightswitch function to turn my hueLight on/off wirelessly.

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const axios = require('axios');
-const fs = require('fs');
-var URL = "GET THE URL HERE WHEN HOME";
+const axios = require('axios'); // axios is the software used to sent HTTP requests.
+const fs = require('fs'); // fi (fileSystem) is the software used to read between files
 
-let rawdata = fs.readFileSync('colorlibrary.json');
+const USERNAME = "bUrp9C1fJD7CnkOyTILhNJdx-YIKB28PKSfXg0Kk"; // username needed to verify a legitamate request
+const IP = '192.168.1.62'; //IP of the Huebridge, the modem that sends HTTP requests to the lights over local WIFI
+const URL = `http://${IP}/api/${USERNAME}/lights/`; // the full HTTP directory
+
+let rawdata = fs.readFileSync('colorlibrary.json'); // converts .json information from bytes into string form
 let colors = JSON.parse(rawdata);
+
+
+const lightswitch = async (lightId, on) => { //simple lightswitch to test if the information can be sent over WIFI, turns light on / off 
+    try {
+        axios.put(`${URL}${lightId}/state`, {on,});
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+lightswitch(1, true);
