20487748e2870dbdab8eaf74fdb2b043945fe0b2
19cmackenzie <19cmackenzie@wakatipu.school.nz>
2023-06-20 19:28:33 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const axios = require('axios'); // axios is the software used to sent HTTP requests.
 const fs = require('fs'); // fi (fileSystem) is the software used to read between files
 
-const USERNAME = "bUrp9C1fJD7CnkOyTILhNJdx-YIKB28PKSfXg0Kk"; // username needed to verify a legitamate request
+const USERNAME = "bUrp9C1fJD7CnkOyTILhNJdx-YIKB28PKSfXg0Kk"; // username needed to verify a legitamate request specific only to my hueBridge
 const IP = '192.168.1.62'; //IP of the Huebridge, the modem that sends HTTP requests to the lights over local WIFI
 const URL = `http://${IP}/api/${USERNAME}/lights/`; // the full HTTP directory
 
@@ -11,10 +11,11 @@ let colors = JSON.parse(rawdata);
 
 const lightswitch = async (lightId, on) => { //simple lightswitch to test if the information can be sent over WIFI, turns light on / off 
     try {
-        axios.put(`${URL}${lightId}/state`, {on,});
+        axios.put(`${URL}${lightId}/state`, {on, hue: 5000, sat: 255, bri:255,});
+        console.log();
     } catch (err) {
         console.error(err);
     }
 };
 
-lightswitch(1, false);
+lightswitch(1, true);
