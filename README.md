99913c2e691f60e84935a8ea7fb36460576bbac9
19cmackenzie <19cmackenzie@wakatipu.school.nz>
2023-06-29 14:17:43 +1200

    Sprint 1 release
    separated brightness parameter into its own function, created a fade effect, and changed the variable names to be more apropriate

--- README.md
+++ README.md
@@ -1,13 +1,2 @@
 # PHiLS
  Phillpis Hue Light System, my 3 month digital technologies assessment
-
-==========================
-colorlibrary.json
-
-this is a database that stores the hue value for each color, 
-it assigns each value to a name, (i.e red has a hue value of 0). this helps to easily 
-specify what colors are required.
-==========================
-index.js
-
-The javascript file that manages all the functions and commands that are sent out to the hueLights
--- api.js
+++ api.js
@@ -1,4 +1,5 @@
 const axios = require('axios'); // axios is the software used to sent HTTP requests.
+const { randomInt } = require('crypto');
 const fs = require('fs'); // fs (fileSystem) is the software used to read between files
 
 const USERNAME = "bUrp9C1fJD7CnkOyTILhNJdx-YIKB28PKSfXg0Kk"; // username needed to verify a legitamate request specific only to my hueBridge
@@ -18,20 +19,37 @@ function lightswitch(lightId, onOff) { //simple lightswitch to send information 
     }
 };
 
-function sendData(lightId, func) { // Sends colorData/ other data provided by other light manipulation programmes
+function setColor(lightID, color) { // Sends colorData; other data provided by other light manipulation programmes
     try {
-        let hue = library.colors[func].hue; //index hue value from library.json
-        let sat = library.colors[func].sat; //index saturation value from library.json
-        let bri = library.colors[func].bri; //index saturation value from library.json
+        let hue = library.colors[color].hue; 
+        let sat = library.colors[color].sat; 
 
-        axios.put(`${URL}${lightId}/state`, {hue:hue, sat:sat, bri:bri,}); 
-        console.log(lightId, hue, sat, bri); //DEBUG
+        axios.put(`${URL}${lightID}/state`, {hue:hue, sat:sat,}); 
+        console.log(lightID, hue, sat); //DEBUG
     } catch (err) {
         console.error(err);
     }
 };
 
-lightswitch(1, true)
-sendData(1, 'white'); //change light color data
+function brightness (lightID, brightness) {
+    let bri = brightness;
+    axios.put(`${URL}${lightID}/state`, {bri:bri});
+
+}
+function fade(lightID, targetColor, transitionTime){ // fade between light colors
+    let hue = library.colors[targetColor].hue; //index color library for hue value of desired color
+    axios.put(`${URL}${lightID}/state`, {hue:hue, transitiontime: transitionTime}); //send axios PUT command to hueBridge
+};
+
+// below are some values that call these defined fuctions. feel free to have fun using them.
+
+lightswitch(1, true) //lightID, on/off state
+fade(1, "orange", 100) //lightID, Color, transitiontime(milliseconds)
+setColor(1, "orange") //lightID, Color
+brightness(1, 205) //light ID, brightness (1-255)
+
+
+
+
 
 
